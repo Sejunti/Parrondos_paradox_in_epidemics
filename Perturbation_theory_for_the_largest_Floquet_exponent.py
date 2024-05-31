5598adf1e505734530aa14eb67e1def4a22f4f3e
Sejunti <96703735+Sejunti@users.noreply.github.com>
2024-05-31 14:42:42 -0400

    Add files via upload

--- Perturbation_theory_for_the_largest_Floquet_exponent.py
+++ Perturbation_theory_for_the_largest_Floquet_exponent.py
@@ -1,49 +1,38 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-
 import numpy as np
 import matplotlib.pyplot as plt
-get_ipython().run_line_magic('pylab', 'inline')
 from scipy.integrate import odeint
 import sympy as sp
-sp.init_printing()
 from scipy import linalg as la
 import math
 from scipy.linalg import eig
 from SIS_Model_Over_Perioic_Temporal_Networks import *  
 
-
-
-
 def approximate_matrix(beta,mu,T,barA):
     """
-    Function for finding the first-order approximation of  matrix T. 
+    Function computes the first-order approximation of matrix, T. 
     Parameters:
               beta(float):Infection rate
               mu(float):Recovery rate
-              T(integer): Period 
-              barA(numpy array): Average adjacency matrix
+              T(integer):Period 
+              barA(numpy array):Time-averaged adjacency matrix
     Returns:
-           barT(numpy array): First order approximation of  matrix T
+           barT(numpy array): First order approximation of  matrix, T
     """ 
     c=matrix_exponetial(-T*mu*I)
     h=I+(beta*T*barA)
     barT=np.dot(h,c)
     return barT
 
-
 ###################################################################
 
 def approximate_largest_eigenvalue(beta,mu,T,barA):
     """
-    Function for finding the first-order approximation of the largest eigenvalue of T. 
+    Function computes the first-order approximation of the largest eigenvalue of matrix, T. 
     Parameters:
               beta(float):Infection rate
               mu(float):Recovery rate
-              T(integer): Period 
-              barA(numpy array): Average adjacency matrix
+              T(integer):Period 
+              barA(numpy array):Time-averaged adjacency matrix
     Returns:
            approximate_max_eigen_value(float): First order approximation of largest eigenvalue of T
     """ 
@@ -53,18 +42,16 @@ def approximate_largest_eigenvalue(beta,mu,T,barA):
     approximate_max_eigen_value=a+beta*max_eigen_value*T*a
     return approximate_max_eigen_value
 
-
 ###################################################################
-
-
 
 def approximate_lamda_F(beta,mu,T,barA):
     """
-    Function for finding the first-order approximation of the Floquet exponent. 
+    Function computes the first-order approximation of the largest Floquet exponent. 
     Parameters:
               beta(float):Infection rate
               mu(float):Recovery rate
-              barA(numpy array): Average adjacency matrix
+              T(integer):Period 
+              barA(numpy array):Time-averaged adjacency matrix
     Returns:
            approximate_lamda_F(float): First order approximation of the Floquet exponent
     """ 
