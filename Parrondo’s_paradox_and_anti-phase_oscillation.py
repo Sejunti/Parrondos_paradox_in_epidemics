5b3b6f322ef8af938ecbd7ab5929b8c351231094
Sejunti <96703735+Sejunti@users.noreply.github.com>
2024-06-13 01:07:47 -0400

    Add files via upload

--- Parrondo’s_paradox_and_anti-phase_oscillation.py
+++ Parrondo’s_paradox_and_anti-phase_oscillation.py
@@ -1,37 +1,22 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-
-
 import numpy as np
 import matplotlib.pyplot as plt
-get_ipython().run_line_magic('pylab', 'inline')
 from scipy.integrate import odeint
 import sympy as sp
-sp.init_printing()
 from scipy import linalg as la
 import math
 from Simulation_of_SIS_Model_Over_Perioic_Temporal_Network import * 
 
-
-#########################################################################
-
-
-def fraction_anti_phase(z, times, t_start, t_end):
+def fraction_anti_phase(z):
     """
-    Function for measure  the anti-phase oscillation.
-    Parameters:
+    Function measures the extent of anti-phase oscillation by the fraction of time.
+    Parameter:
               z(numpy array): State of the system at each time step
-              times(numpy array): Time step
-              t_start(integer): Start of time interval
-              t_end(integer): End of time interval
     Returns:
-            q(float): Anti-phase oscillation 
+            q(float): Fraction of time  
     """
 
-    time_interval = (times >= t_start) & (times <= t_end)
     times, z = get_trajectory(beta,mu,As,periods,durations,z0)
+    time_interval = (times >= t_start) & (times <= t_end)
     z_interval = z[time_interval]
     q=(np.sum(np.diff(z_interval[:, 0]) * np.diff(z_interval[:, 1]) < 0)) / len(z_interval[:, 0])
     return q
